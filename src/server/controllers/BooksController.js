3d37054aedbf67dbe828f0325df092690d93ff4c
root <375262986@qq.com>
2019-07-24 15:16:39 +0800

    添加bigpipe quicklink

--- src/server/controllers/BooksController.js
+++ src/server/controllers/BooksController.js
@@ -1,6 +1,6 @@
 import cheerio from 'cheerio'
 import { route, GET } from 'awilix-koa'
-import { get } from 'https';
+import {Readable} from 'stream'
 
 @route('/books')
 class BooksController {
@@ -16,27 +16,39 @@ class BooksController {
     @GET()
     async actionList(ctx, next) {
         const result = await this.booksService.getData()
-        console.log(result)
         const html = await ctx.render('books/pages/list', {
             data: result.data
         });
         if (ctx.request.header['x-pjax']) {
+            ctx.status = 200;
+            ctx.type = 'html'
             console.log('站内')
             const $ = cheerio.load(html)
-            // ctx.body = $('#js-hooks-data').html()
-            let _result = ''
             $('.pjaxcontext').each(function() {
-                _result += $(this).html()
+                ctx.res.write($(this).html())
             })
 
             $('.lazyload-js').each(function() {
-                _result += `<script src="${$(this).attr("src")}"></script>`
+                //核心basket.js
+                //ctx.res.write(`<script>activeJS("${$(this).attr("src")}")</script>`);
+                ctx.res.write(`<script src="${$(this).attr("src")}"></script>`);
             })
-            console.log($('.pjaxcontext'))
-            ctx.body = _result
+            ctx.res.end();
         } else {    
             console.log('直接刷')
-            ctx.body = html
+            function createSSRStreamPromise () {
+                return new Promise((resolve, reject) => {
+                    const htmlStream = new Readable()
+                    htmlStream.push(html)
+                    htmlStream.push(null)
+                    ctx.status = 200
+                    ctx.type = 'html'
+                    htmlStream.on('error', err => {
+                        reject(err)
+                    }).pipe(ctx.res)
+                })
+            }
+            await createSSRStreamPromise()
         }
     }
 }
